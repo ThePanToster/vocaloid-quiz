f0c085ec600fcc9a0c2db1ffe4547b7b282a7963
ThePanToster <alcrafter@o2.pl>
2022-06-28 01:11:09 +0200

    Layout change - Added and placed most of components in the game - CSS keyframes added (todo: animations in .ts) - Page refreshing methods added to GameComponent

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core'
-
+import { Router } from '@angular/router'
+import { EventData, GestureEventData, View } from '@nativescript/core'
+import * as GameEngine from "./gameEngine"
 @Component({
   selector: 'ns-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent {}
+export class GameComponent {
+    constructor(private router: Router) {}
+    pageLoaded(args: EventData){
+        const view = args.object as View
+        const page = view.page
+        // console.log("zaladowano strone")
+    }
+    onTap(args: GestureEventData) {
+        const view = args.object as View
+        const page = view.page
+        const resultsElem = page.getViewById("results")
+
+        if(resultsElem.style.visibility != "hidden"){
+            GameEngine.restartPage(this.router)
+        }
+    }
+}
